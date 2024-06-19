7a4d3a201717f063f9d83cad68cd4c8110c5b976
Nana Quam <quamgyambrah@gmail.com>
2024-06-19 15:22:37 -0400

    minor fix

--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -34,12 +34,12 @@ async function SearchTerm({
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
         {similarMovies.map((movie, i) => (
-          <div className="flex space-x-2 relative">
+          <div key={movie._id} className="flex space-x-2 relative">
             <p className="absolute flex items-center justify-center left-4 top-2 text-white font-extrabold text-xl z-40 rounded-full bg-indigo-500/80 w-10 h-10">
               {i + 1}
             </p>
 
-            <MoviePoster key={movie._id} movie={movie} />
+            <MoviePoster movie={movie} />
           </div>
         ))}
       </div>
